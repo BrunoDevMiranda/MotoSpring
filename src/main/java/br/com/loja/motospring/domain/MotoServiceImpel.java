a36ed8c2d4850dc4ba1d2d8f7d7e6329e2774ab8
BrunoDevMiranda <mirandabsm.dev@gmail.com>
2023-03-03 14:13:05 -0300

    Adicionando create,update,findById, findAll e delete. no MotoServiceImpel

--- src/main/java/br/com/loja/motospring/domain/MotoServiceImpel.java
+++ src/main/java/br/com/loja/motospring/domain/MotoServiceImpel.java
@@ -1,32 +1,57 @@
 package br.com.loja.motospring.domain;
 
 import br.com.loja.motospring.adapter.dto.MotoDto;
+import br.com.loja.motospring.adapter.mapper.MotoMapper;
+import br.com.loja.motospring.data.model.MotoModel;
+import br.com.loja.motospring.data.repository.MotoRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class MotoServiceImpel implements MotoService {
+
+    private final MotoRepository repository;
+    private final MotoMapper mapper;
+
+    public MotoServiceImpel(MotoRepository repository, MotoMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
     @Override
     public MotoDto create(MotoDto motoDto) {
-        return null;
+        MotoModel moto = mapper.toEntity(motoDto);
+        MotoModel motoSaved = repository.save(moto);
+        return mapper.toDto(motoSaved);
     }
 
     @Override
     public MotoDto update(MotoDto motoDto) {
-        return null;
+        MotoModel moto = mapper.toEntity(motoDto);
+        MotoModel motoUpdate = repository.save(moto);
+        return mapper.toDto(motoUpdate);
     }
 
     @Override
     public MotoDto findById(Long id) {
-        return null;
+        Optional<MotoModel> motoId = repository.findById(id);
+        return mapper.toDto(motoId.get());
     }
 
     @Override
     public List<MotoDto> getAll() {
-        return null;
+        List<MotoModel> motoModels = repository.findAll();
+        List<MotoDto> motoDtos = new ArrayList<>();
+        for (MotoModel model : motoModels){
+            motoDtos.add(mapper.toDto(model));
+        }
+        return motoDtos;
     }
 
     @Override
     public void delete(Long id) {
+        repository.deleteById(id);
 
     }
 }
