b0d9ada2139831bd78f2a7d62d43935aac2acc5f
BrunoDevMiranda <mirandabsm.dev@gmail.com>
2023-03-03 14:00:30 -0300

    Criação da Entidade MotoDto

---
+++ src/main/java/br/com/loja/motospring/adapter/dto/MotoDto.java
@@ -0,0 +1,75 @@
+package br.com.loja.motospring.adapter.dto;
+
+import jakarta.persistence.Column;
+
+public class MotoDto {
+    private Long id;
+
+    private String modelo;
+
+    private String marca;
+
+    private String tipo;
+
+    private int ano;
+
+    private double preco;
+
+    private String foto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
